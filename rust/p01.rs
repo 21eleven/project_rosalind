cb9f6faf29f295d64999df6ee640709c7ed8d534
Noah Lidell <noahlidell@gmail.com>
2018-12-25 15:04:48 -0600

    initial commit, git ignore, problem 1 in rust

---
+++ rust/p01.rs
@@ -0,0 +1,25 @@
+use std::fs;
+
+fn main() {
+    let target = "./target.txt";
+    let dna = fs::read_to_string(target)
+        .expect(&format!("error reading {}", target));
+
+    let mut a: usize = 0;
+    let mut c: usize = 0;
+    let mut g: usize = 0;
+    let mut t: usize = 0;
+    
+    for nucleotide in dna.chars() {
+        match nucleotide {
+            'A' => a += 1,
+            'C' => c += 1,
+            'G' => g += 1,
+            'T' => t += 1,
+            _ => (),
+        }
+    }
+
+    println!("{} {} {} {}", a, c, g, t);
+}
+    
